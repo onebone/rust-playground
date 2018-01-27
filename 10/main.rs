881b6911c860fccc3cb0a830d1a4e68460f9839b
onebone <jyc00410@gmail.com>
2018-01-27 20:10:19 +0900

    우와 신기해

---
+++ 10/main.rs
@@ -0,0 +1,26 @@
+macro_rules! foo {
+    ($x:expr) => (println!("Hello World X {}", $x));
+//    (y => $y:expr) => (println!("Hello World Y {}", $y));
+}
+
+macro_rules! bar {
+    ( $( $x:expr ),* ) => {
+        $(
+            println!("{}", $x);
+        )*
+    };
+}
+
+macro_rules! foobar {
+    ( $( {$x:expr} ),*) => {
+        $(
+            println!("{}", $x);
+        )*
+    };
+}
+
+fn main(){
+    foo!(1); 
+    bar!(1, 2, 3);
+    foobar!({4}, {5}, {6});
+}
